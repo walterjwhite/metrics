07ae442676e8e77a5450aa51266377dbc6531d13
Walter White <WalterWhite@walterjwhite.com>
2018-04-06 07:51:25 -0400

    updated / merged

--- core/src/main/java/com/walterjwhite/infrastructure/metrics/api/annotation/Tag.java
+++ core/src/main/java/com/walterjwhite/infrastructure/metrics/api/annotation/Tag.java
@@ -3,7 +3,8 @@ package com.walterjwhite.infrastructure.metrics.api.annotation;
 import java.lang.annotation.*;
 
 /**
- * TODO: redesign - Rather than assign tags at compile-time, maintain an internal map / lookup table (for a given unique tag name, these tags apply)
+ * TODO: redesign - Rather than assign tags at compile-time, maintain an internal map / lookup table
+ * (for a given unique tag name, these tags apply)
  */
 @Deprecated
 @Target({ElementType.METHOD, ElementType.CONSTRUCTOR})
--- modules/prometheus/src/main/java/com/walterjwhite/metrics/modules/prometheus/property/PrometheusPrefix.java
+++ modules/prometheus/src/main/java/com/walterjwhite/metrics/modules/prometheus/property/PrometheusPrefix.java
@@ -1,6 +1,6 @@
 package com.walterjwhite.metrics.modules.prometheus.property;
 
-// import com.walterjwhite.google.guice.property.property.DefaultValue;
+// import com.walterjwhite.google.guice.property.annotation.DefaultValue;
 // import com.walterjwhite.google.guice.property.property.GuiceProperty;
 
 // public interface PrometheusPrefix extends GuiceProperty {
