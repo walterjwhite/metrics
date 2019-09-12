a5fec2653e1fe8ebe6fca3337e303f9b35fc7936
Walter White <WalterWhite@walterjwhite.com>
2019-09-11 23:29:12 -0400

    merged

--- modules/netflix-atlas/impl/src/main/java/com/walterjwhite/metrics/modules/prometheus/DefaultAtlasConfig.java
+++ modules/netflix-atlas/impl/src/main/java/com/walterjwhite/metrics/modules/prometheus/DefaultAtlasConfig.java
@@ -3,6 +3,7 @@ package com.walterjwhite.metrics.modules.prometheus;
 import com.netflix.spectator.atlas.AtlasConfig;
 
 public class DefaultAtlasConfig implements AtlasConfig {
+  // TODO: this should use properties and not be hard-coded
   @Override
   public String uri() {
     return "http://localhost:7101/api/v1/publish";
--- modules/netflix-atlas/impl/src/main/java/com/walterjwhite/metrics/modules/prometheus/property/NetflixStep.java
+++ modules/netflix-atlas/impl/src/main/java/com/walterjwhite/metrics/modules/prometheus/property/NetflixStep.java
@@ -1,9 +1,11 @@
 package com.walterjwhite.metrics.modules.prometheus.property;
 
 import com.walterjwhite.property.api.annotation.DefaultValue;
+import com.walterjwhite.property.api.annotation.PropertyValueType;
 import com.walterjwhite.property.api.property.ConfigurableProperty;
 import java.time.Duration;
 
+@PropertyValueType(Duration.class)
 public interface NetflixStep extends ConfigurableProperty {
   @DefaultValue Duration Default = Duration.ofSeconds(10);
 }
